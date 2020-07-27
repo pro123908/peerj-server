ff0480a10e10e597a4b93000362dfc5ecc5c184e
Bilal Ahmad <pro123908@gmail.com>
2020-07-28 00:13:42 +0500

    server code fixed

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var PeerServer = require("peer").PeerServer;
 var express = require("express");
 var cors = require("cors");
 var app = express();
 
 app.use(cors());
-
-var server = PeerServer({ port: 9000, path: "/" });
-console.log(server);
+var port = process.env.PORT || 9000;
+srv = app.listen(port);
+app.use(
+  "/peerjs",
+  require("peer").ExpressPeerServer(srv, {
+    debug: true,
+  })
+);
