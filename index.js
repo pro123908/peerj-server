e1cc0f958d632976214fdb3784ad325018a6b4fe
Bilal Ahmad <pro123908@gmail.com>
2020-07-28 00:07:32 +0500

    cors added

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var PeerServer = require("peer").PeerServer;
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+app.use(cors());
 
 var server = PeerServer({ port: 9000, path: "/" });
 console.log(server);
